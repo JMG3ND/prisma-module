23138e8cc76126550995ee6ca379cd00de249aa1
Jose Miguel <jo0303se@hotmail.com>
2025-10-15 14:34:43 -0500

    feat: refactor Prisma configuration and module integration for improved performance

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,13 +1,5 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   devtools: { enabled: true },
-  nitro: {
-    experimental: {
-      wasm: true,
-    },
-    externals: {
-      inline: ['@prisma/client'],
-    },
-  },
-  myModule: {},
+  compatibilityDate: '2025-10-15',
 })
