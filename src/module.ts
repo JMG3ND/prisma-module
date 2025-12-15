c25d5e8fce80fe781575be9eb3aed8b5aa174321
Jose Miguel <jo0303se@hotmail.com>
2025-12-15 15:42:29 -0500

    feat: define MyModuleOptions interface and update setup to use dynamic routePrismaDir

--- src/module.ts
+++ src/module.ts
@@ -1,18 +1,26 @@
 import { defineNuxtModule, createResolver, addServerImportsDir } from '@nuxt/kit'
 import { checkLibFile, configPrismaForNuxt } from './config'
 
-export default defineNuxtModule({
+interface MyModuleOptions {
+  routePrismaDir: string
+}
+
+export default defineNuxtModule<MyModuleOptions>({
   meta: {
     name: 'my-module',
     configKey: 'myModule',
   },
   // Default configuration options of the Nuxt module
-  defaults: {},
-  setup(_options, nuxt) {
+  defaults: {
+    /* default options */
+    routePrismaDir: 'lib/prisma.ts',
+  },
+  setup(options, nuxt) {
     const { resolve } = createResolver(import.meta.url)
 
     const rootDir = nuxt.options.rootDir
-    const prismaFileDir = resolve(rootDir, 'lib/prisma.ts')
+    const prismaFileDir = resolve(rootDir, options.routePrismaDir)
+
     checkLibFile(prismaFileDir)
 
     addServerImportsDir(prismaFileDir)
